dd3d39164e992ba33812f13294d40fa9fc79cb50
Willi <willi.gautier@gisaia.com>
2024-12-18 14:39:43 +0100

    Change generated docs folder creation

--- mkDocs.sh
+++ mkDocs.sh
@@ -1,7 +1,7 @@
 #!/bin/sh -e
 
 ## CREATE TARGET DIRECTORY ##
-rm -rf target/generated-docs
-mkdir target/generated-docs
+mkdir -p target/generated-docs
+rm -rf target/generated-docs/*
 
 cp -r docs/docs/* target/generated-docs
